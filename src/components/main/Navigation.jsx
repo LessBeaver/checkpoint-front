32cca7a999be4a96eba706c862e9ea2fbe36864e
Leslie <lesliec38@hotmail.fr>
2020-07-22 16:16:22 +0200

    use of bootstrap, navbar ok

--- src/components/main/Navigation.jsx
+++ src/components/main/Navigation.jsx
@@ -1,43 +1,73 @@
-import React, { useState } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarText
-} from "reactstrap";
+import React from "react";
 
-export default function Navigation() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
+export default function NAvigation() {
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Portfolio</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/accueil">Accueil</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/mes-voyages">Portfolio</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/a-propos">A Propos</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/recherce">Recherche</NavLink>
-            </NavItem>
-          </Nav>
-          <NavbarText>Leslie Rouzier</NavbarText>
-        </Collapse>
-      </Navbar>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">
+          Portfolio
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/accueil">
+                Accueil<span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/a-propos">
+                A propos
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Recherche
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <form class="form-inline my-2 my-lg-0">
+                  <input
+                    class="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button
+                    class="btn btn-outline-success my-2 my-sm-0"
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </form>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
