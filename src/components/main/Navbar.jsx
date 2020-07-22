4f11b149db38a229de1511548ecd91c8828c7f5c
Leslie <lesliec38@hotmail.fr>
2020-07-22 15:44:45 +0200

    react strap navbar into navbar component

---
+++ src/components/main/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from "reactstrap";
+
+const Navbar = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Portfolio</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/accueil">Accueil</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/mes-voyages">Portfolio</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/a-propos">A Propos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/recherce">Recherche</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Leslie Rouzier</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Example;
