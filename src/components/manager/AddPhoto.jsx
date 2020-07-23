5a584dfe30ba4e12cc9ceebccae4fd45d5365043
Leslie <lesliec38@hotmail.fr>
2020-07-23 13:20:51 +0200

    get voyage to select one done

--- src/components/manager/AddPhoto.jsx
+++ src/components/manager/AddPhoto.jsx
@@ -3,11 +3,20 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    widht: '100%'
+  },
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -27,18 +36,29 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function AddTrip() {
+export default function AddPhoto() {
   const classes = useStyles();
-  const [image, setImages] = useState('');
-  const [name, setNames] = useState('');
+  const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [tripName, setTripName] = useState('');
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/trip`)
+      .then(res => res.data)
+      .then(res => setTrips(res))
+      .catch(e => {
+        alert(`Erreur lors de la récupération des images ${e.message}`);
+      });
+  }, []);
 
   const handleClickImage = e => {
     e.preventDefault();
-    const url = 'http://localhost:4000/trip';
     axios
-      .post(url, { picture_url: image, name })
+      .post(`http://localhost:4000/photo/${tripName}`)
       .then(res => res.data)
-      .then(res => setImages(res))
+      .then(res => setTripName(res))
       .catch(e => {
         alert(`Erreur lors de la récupération des images ${e.message}`);
       });
@@ -49,9 +69,25 @@ export default function AddTrip() {
       <CssBaseline />
       <div className={classes.paper}>
         <Typography component="h1" variant="h5">
-          Ajouter un voyage
+          Ajouter des images
         </Typography>
         <form className={classes.form} onSubmit={e => handleClickImage(e)}>
+          <FormControl variant="outlined" className={classes.root}>
+            <InputLabel id="trip">Sélectionner un voyage</InputLabel>
+            <Select
+              id="name"
+              label="Nom du voyage"
+              name="name"
+              value={tripName}
+              onChange={e => setTripName(e.target.value)}
+            >
+              {trips.map(({ id_trip, name }) => (
+                <MenuItem value={id_trip} key={id_trip}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <TextField
             variant="outlined"
             margin="normal"
@@ -62,8 +98,21 @@ export default function AddTrip() {
             name="name"
             autoComplete="name"
             autoFocus
+            value={tripName}
+            onChange={e => setTripName(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Nom de l'image"
+            name="name"
+            autoComplete="name"
+            autoFocus
             value={name}
-            onChange={e => setNames(e.target.value)}
+            onChange={e => setName(e.target.value)}
           />
           <TextField
             variant="outlined"
@@ -76,7 +125,7 @@ export default function AddTrip() {
             autoComplete="picture_url"
             autoFocus
             value={image}
-            onChange={e => setImages(e.target.value)}
+            onChange={e => setImage(e.target.value)}
           />
           <Button
             type="submit"
