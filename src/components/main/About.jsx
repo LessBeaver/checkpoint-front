75fe983c006d5008ae1c60086679b67d42e67ad8
Leslie <lesliec38@hotmail.fr>
2020-07-24 08:58:43 +0200

    responsive accueil 1100 to 1200px

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -5,7 +5,7 @@ import './mainqueries.css';
 export default function About() {
   return (
     <div>
-      <div className="about-image" style={{ height: '95vh', width: 'auto' }}></div>
+      <div className="about-image"></div>
       <div>
         <img src="https://zupimages.net/up/20/30/64ch.jpg" alt="" className="about-image-perso" />
       </div>
